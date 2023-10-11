7b1038ce6ca41e3aa174bfb00592f7e4c23c6b29
kHushisali <128462293+kHushisali@users.noreply.github.com>
2023-10-11 13:31:41 +0000

    01_variables.js

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 14453
+let accountEmail ="khushisali678@gmail.com"
+var accountPassword = "1234"
+ accountCity = "khandwa"
+
+
+accountEmail = "khushi5624"
+accountPassword ="12121"
+accountCity ="pune"
+
+//nconsole.log(accountID);
+console.table([accountId,accountEmail,accountPassword,accountCity]);
