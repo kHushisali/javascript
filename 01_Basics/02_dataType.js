65379832a08dd66af47b812ea1f38982274a2aa1
kHushisali <128462293+kHushisali@users.noreply.github.com>
2023-10-11 18:16:53 +0000

    datatype

---
+++ 01_Basics/02_dataType.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all js code newer version
+//alert(3+3) you are using node js not browser
+
+console.log(3+3)//code readability should be highly
+
+let name ="khushi"
+let age = 18
+let  isloggIn = false
+
+//name of the datatype
+//number -> 2 power of 53
+//bigint
+//string//boolean -> true and false
+//null => standlone value
+//undefined =>
+//symbol =>unique
+//object
+
+console.log(type of undefined); //undefined
+console.log(typeof null); //object 
